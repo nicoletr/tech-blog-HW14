728224ee2cdbaf60b8043e59406e40ac80d8e564
Nicoletr <nicole.trpevski@gmail.com>
2021-12-20 13:10:10 +0800

    import updates

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,2 @@
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models");
--- controllers/index.js
+++ controllers/index.js
@@ -7,8 +7,8 @@ router.use('/', homepageRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
 
-router.use((req, res) => {
-    res.status(404).end();
-});
+// router.use((req, res) => {
+//     res.status(404).end();
+// });
 
 module.exports = router;
